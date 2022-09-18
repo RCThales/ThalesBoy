b581f99b2ff8931abbe6c66af270959fe007406f
Thales Cardoso <canadathales@gmail.com>
2022-09-18 01:09:49 -0400

    Added TYPESCRIPT to the project.

---
+++ src/script.ts
@@ -0,0 +1,21 @@
+
+const gameOnAndOff = () => {
+
+    const light:HTMLElement|null = document.getElementById('led')
+    const screen:HTMLElement|null = document.getElementById('iframeScreen')
+
+    if(light !== null && screen !== null){
+        //If videogame is on
+        if (light.classList.contains('on')) {
+            screen.style.backgroundColor = 'rgb(105, 116, 99)';
+            light.style.backgroundColor = 'rgb(34, 34, 34)'
+            light.classList.remove('on')
+        }
+        //If videogame is off
+        else {
+            screen.style.backgroundColor = 'rgb(208, 228, 197)';
+            light.style.backgroundColor = 'red'
+            light.classList.add('on')
+        } 
+    }
+}
