20791407cd0ffc5a8f3feb24135a7f3ee31f2053
Thales Cardoso <46228954+RCThales@users.noreply.github.com>
2024-03-01 23:25:30 +0000

    Renamed `events` to `event_triggers`

---
+++ src/event_triggers.ts
@@ -0,0 +1,82 @@
+import {
+  toggleBetweenDayAndNightTheme,
+  makeCurrentColorButtonInvisible,
+  makePreviousColorButtonVisible,
+} from "./index.js";
+
+import keys from "./key_codes.js";
+
+import {
+  POWER_BTN_DESKTOP,
+  POWER_BTN_MOBILE,
+  THEME_BUTTON,
+  GAME_PAD_UP_BUTTON,
+  GAME_PAD_DOWN_BUTTON,
+  GAME_PAD_LEFT_BUTTON,
+  GAME_PAD_RIGHT_BUTTON,
+  GAME_PAD_POSITIVE_BUTTON,
+  GAME_PAD_NEGATIVE_BUTTON,
+  GAME_PAD_START_BUTTON,
+  GAME_PAD_SELECT_BUTTON,
+  CONSOLE_COLOR_BUTTONS,
+} from "./clickable_elements.js";
+import Console from "./console.js";
+
+const consoleInstance = new Console();
+window.addEventListener("load", () => {
+  consoleInstance.setConsoleColorOnStartUp();
+});
+
+CONSOLE_COLOR_BUTTONS.addEventListener("pointerup", (event) => {
+  const target = event?.target as HTMLButtonElement;
+  const colorClickedName = target.id.toString();
+  console.log(colorClickedName);
+  makePreviousColorButtonVisible();
+  consoleInstance.changeConsoleColor(colorClickedName);
+  makeCurrentColorButtonInvisible(target);
+});
+
+POWER_BTN_DESKTOP.addEventListener("pointerdown", () => {
+  if (!consoleInstance.isConsoleOn) {
+    consoleInstance.turnConsoleOn();
+  } else {
+    consoleInstance.turnConsoleOff();
+  }
+});
+POWER_BTN_MOBILE.addEventListener("pointerdown", () => {
+  if (!consoleInstance.isConsoleOn) {
+    consoleInstance.turnConsoleOn();
+  } else {
+    consoleInstance.turnConsoleOff();
+  }
+});
+
+THEME_BUTTON.addEventListener("pointerup", () => {
+  toggleBetweenDayAndNightTheme();
+});
+
+const { up, left, right, down, select, start, positive, negative } = keys;
+GAME_PAD_UP_BUTTON.addEventListener("pointerdown", () => {
+  consoleInstance.dispacthClickEventsToConsoleScreenIFrame(up);
+});
+GAME_PAD_DOWN_BUTTON.addEventListener("pointerdown", () => {
+  consoleInstance.dispacthClickEventsToConsoleScreenIFrame(down);
+});
+GAME_PAD_LEFT_BUTTON.addEventListener("pointerdown", () => {
+  consoleInstance.dispacthClickEventsToConsoleScreenIFrame(left);
+});
+GAME_PAD_RIGHT_BUTTON.addEventListener("pointerdown", () => {
+  consoleInstance.dispacthClickEventsToConsoleScreenIFrame(right);
+});
+GAME_PAD_POSITIVE_BUTTON.addEventListener("pointerdown", () => {
+  consoleInstance.dispacthClickEventsToConsoleScreenIFrame(positive);
+});
+GAME_PAD_NEGATIVE_BUTTON.addEventListener("pointerdown", () => {
+  consoleInstance.dispacthClickEventsToConsoleScreenIFrame(negative);
+});
+GAME_PAD_START_BUTTON.addEventListener("pointerdown", () => {
+  consoleInstance.dispacthClickEventsToConsoleScreenIFrame(start);
+});
+GAME_PAD_SELECT_BUTTON.addEventListener("pointerdown", () => {
+  consoleInstance.dispacthClickEventsToConsoleScreenIFrame(select);
+});
