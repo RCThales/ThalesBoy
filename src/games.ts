2c4d4a1655905f81e492aff5147ed79a8fda9377
Fernando Rodrigues Cardoso <f_cardoso@fanshaweonline.ca>
2024-03-02 21:23:40 +0000

    Refactoring `menu.ts` file

--- src/games.ts
+++ src/games.ts
@@ -1,12 +1,39 @@
-const games: object[] = [
-  {
-    name: "Green Snake",
-    id: 1,
-  },
-  {
-    name: "Blue Snake",
-    id: 2,
-  },
-];
+export class Game {
+  private _name: string;
+  private _id: number;
+  private _gameHTML: string;
 
-export default games;
+  constructor(name: string, id: number) {
+    this._name = name;
+    this._id = id;
+    this._gameHTML = `game_${id}`;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public get gameHTML(): string {
+    return this._gameHTML;
+  }
+}
+
+export class GamesList {
+  private games: Game[] = [];
+  static instance: GamesList;
+
+  constructor() {
+    if (GamesList.instance) {
+      return GamesList.instance;
+    }
+    return this;
+  }
+
+  addGame(game: Game) {
+    this.games.push(game);
+  }
+}
