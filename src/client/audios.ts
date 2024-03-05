47461430307df1e3c73fe9ca8b56aaa248e59264
Thales Cardoso <canadathales@gmail.com>
2024-03-05 16:48:22 -0500

    Added the `start game` audio to the class

--- src/client/audios.ts
+++ src/client/audios.ts
@@ -3,6 +3,7 @@ class ConsoleAudio {
   private consoleOnAudio = new Audio("../public/audio/consoleOnAudio.mp3");
   private powerButonAudio = new Audio("../public/audio/powerButtonAudio.mp3");
   private settingsInspectAudio = new Audio("../public/audio/inspect.wav");
+  private startGameAudio = new Audio("../public/audio/startgame.wav");
   constructor() {
     if (ConsoleAudio.instance) {
       return ConsoleAudio.instance;
@@ -20,14 +21,22 @@ class ConsoleAudio {
   };
 
   public playInspectAudio = () => {
+    this.settingsInspectAudio.currentTime = 0;
     this.settingsInspectAudio.play();
   };
 
+  public playStartGameAudio = () => {
+    this.startGameAudio.currentTime = 0;
+    this.startGameAudio.play();
+  };
+
   private playPowerButtonAudio = () => {
+    this.powerButonAudio.currentTime = 0;
     this.powerButonAudio.play();
   };
 
   private playConsoleIntroAudio = () => {
+    this.consoleOnAudio.currentTime = 0;
     this.consoleOnAudio.play();
   };
 
@@ -44,6 +53,8 @@ class ConsoleAudio {
   public setConsoleVolume = (volume = 1) => {
     this.consoleOnAudio.volume = volume;
     this.powerButonAudio.volume = volume;
+    this.settingsInspectAudio.volume = volume;
+    this.startGameAudio.volume = volume;
   };
 }
 
