2f4646fce16fa26b7221b040239a471554491040
Thales Cardoso <canadathales@gmail.com>
2024-03-05 16:49:19 -0500

    Added a temporary `caching strategy` so the bandwith on netlify won't get used during development

--- src/client/games.ts
+++ src/client/games.ts
@@ -11,7 +11,7 @@ export class GamesList {
     GamesList.instance = this;
   }
 
-  async fetchGames() {
+  async fetchGamesFromApi() {
     try {
       const response = await fetch("/api/games");
       if (!response.ok) {
@@ -19,10 +19,31 @@ export class GamesList {
       }
       const data = await response.json();
       this._games = data.games;
+      this.cacheGamesList();
     } catch (error) {
       console.error("Error:", error);
       throw error;
     }
+  }
+
+  async fetchGamesFromCache() {
+    this._games = this.getCachedGamesList();
+    if (!this._games) {
+      await this.fetchGamesFromApi();
+    }
+  }
+
+  cacheGamesList() {
+    if (!localStorage.getItem("games")) {
+      localStorage.setItem("games", JSON.stringify(this._games));
+    }
+  }
+
+  getCachedGamesList() {
+    const cachedGamesList = JSON.parse(
+      localStorage.getItem("games") as string,
+    ) as Games[];
+    return cachedGamesList;
   }
 
   get games() {
